c0ae3236eb52f15267c4e9f9fe9a27e071ecf90d
gahui <gahui446@gmail.com>
2024-05-20 17:10:33 +0900

    commit

---
+++ 새 폴더/group/src/router/root.js
@@ -0,0 +1,10 @@
+import {createBrowserRouter } from 'react-router-dom'
+import MainPage from '../pages/main/MainPage';
+
+//router 생성
+const root = createBrowserRouter([
+  { path: "/", element: <MainPage/> }
+]);
+
+// router 내보내기
+export default root;
