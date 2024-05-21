26ddeccb7cf2e5df551c5d8720b2648383ba5953
jun9708 <wnsgud1997@gmail.com>
2024-05-21 19:55:29 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 import { RouterProvider } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import root from './router/root';
 
 function App() {
-  return <RouterProvider router={root}/>
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("/index")
+      .then(res => res.text())
+      .then(m => setMessage(m));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>{message}</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <RouterProvider router={root} />
+    </div>
+  );
 }
 
 export default App;
