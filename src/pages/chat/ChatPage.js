32192ac1a3de37353e14ec0b951edbd67f99e3dd
seungyoon <y01044285848@gmail.com>
2024-05-21 16:16:09 +0900

    add

---
+++ src/pages/chat/ChatPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import DefaultLayout from '../../layouts/DefaultLayout'
+import Example from '../../components/main/Example'
+
+const ChatPage = () => {
+  return (
+    <DefaultLayout>
+       <p>채팅입니다</p>
+    </DefaultLayout>
+  )
+}
+
+export default ChatPage
--- src/router/root.js
+++ src/router/root.js
@@ -1,9 +1,11 @@
 import {createBrowserRouter } from 'react-router-dom'
 import MainPage from '../pages/main/MainPage';
+import ChatPage from '../pages/chat/ChatPage';
 
 //router 생성
 const root = createBrowserRouter([
   { path: "/", element: <MainPage/> },
+  { path: "/chat", element: <ChatPage/> },
 
 ]);
 
