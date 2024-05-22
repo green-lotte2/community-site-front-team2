19056ff59a765092d6ee7c279bf7865e7c8f0ed7
gahui <gahui446@gmail.com>
2024-05-22 16:56:46 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -15,17 +15,6 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <p>{message}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <RouterProvider router={root} />
     </div>
   );
--- src/router/root.js
+++ src/router/root.js
@@ -1,11 +1,13 @@
 import {createBrowserRouter } from 'react-router-dom'
 import MainPage from '../pages/main/MainPage';
 import ChatPage from '../pages/chat/ChatPage';
+import ChatExPage from '../pages/chat/ChatExPage';
 
 //router 생성
 const root = createBrowserRouter([
   { path: "/", element: <MainPage/> },
   { path: "/chat", element: <ChatPage/> },
+  { path: "/chatEx", element: <ChatExPage/> },
 
 ]);
 
