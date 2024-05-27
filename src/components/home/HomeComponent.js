ca7b89146cde204426dc33814456e91d8c834e22
dpsk08270 <dpsk08270@gmail.com>
2024-05-27 17:25:42 +0900

    게시판 리스트 화면구현, 에디터 라이브러리 적용

--- src/components/home/HomeComponent.js
+++ src/components/home/HomeComponent.js
@@ -1,31 +1,29 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom'
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 
 const HomeComponent = () => {
   const authSlice = useSelector((state) => state.authSlice);
   const navigate = useNavigate();
 
-  if(authSlice.username != null){
-    navigate(`/main`)
+  if (authSlice.username != null) {
+    navigate(`/main`);
   }
   return (
-   <div className='Home' >
-      <div className='first_div'>
-        <div className='txt_title'>
-         <img src='../images/logo.png'></img>
-         <h2>일름보에 오신걸 환영합니다.</h2>
+    <div className="Home">
+      <div className="first_div">
+        <div className="txt_title">
+          <img src="../images/logo.png"></img>
+          <h2>일름보에 오신걸 환영합니다.</h2>
         </div>
-        
-        <div className='txt_link'>
-          {!authSlice.username ? ( <>  <Link to="/user/login">로그인</Link></>) : 
-          (<><Link to="/user/logout">로그아웃</Link></> )}
+
+        <div className="txt_link">
+          <Link to="/user/login">로그인</Link>
           <Link to="/user/register">일름보 멤버되기</Link>
         </div>
       </div>
-   </div>
+    </div>
+  );
+};
 
-  )
-}
-
-export default HomeComponent
+export default HomeComponent;
