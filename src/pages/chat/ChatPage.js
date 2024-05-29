1ee24b4fcf0932132ffec1083fca1e807205a528
gahui <gahui446@gmail.com>
2024-05-29 10:11:44 +0900

    commit

--- src/pages/chat/ChatPage.js
+++ src/pages/chat/ChatPage.js
@@ -2,8 +2,14 @@ import React, { useEffect, useState } from 'react';
 import ChatLayout from '../../layouts/ChatLayout';
 import ChatContent from '../../components/chat/ChatContent';
 import createWebSocket from '../../config/createWebSocket';
+import { useSelector } from 'react-redux';
 
 const ChatPage = () => {
+  const authSlice = useSelector((state) => state.authSlice);
+  if(!authSlice.username){
+    alert('잘못된 요청입니다. 로그인 후 이용해주세요.')
+    window.location.href='/main';
+  }
   const [ws, setWs] = useState(null);
   const [isConnected, setIsConnected] = useState(false);
   const [chat, setChat] = useState([]);
