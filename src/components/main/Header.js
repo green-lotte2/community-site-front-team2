11a56dba55fec63624180e85ea67720c78bbb34a
kkome <exazunyan1@gmail.com>
2024-05-29 17:26:55 +0900

    24/05/29 kkome

--- src/components/main/Header.js
+++ src/components/main/Header.js
@@ -6,79 +6,80 @@ import createWebSocket from "../../config/createWebSocket";
 import { useDispatch } from "react-redux";
 import { logout } from "../../slices/authSlice";
 
-
 const Header = (props) => {
   const dispatch = useDispatch();
   const authSlice = useSelector((state) => state.authSlice);
 
   //알림
 
-   const location = useLocation();
+  const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
-  var r = searchParams.get('room'); 
-  if(r == null){
-    r=-1;
+  var r = searchParams.get("room");
+  if (r == null) {
+    r = -1;
   }
 
-
- console.log(props.ws +"이거를 보자 해더")
+  console.log(props.ws + "이거를 보자 해더");
   var ws = props.ws;
   var [chatAll, setChatAll] = useState([]);
-  if(ws != null){
+  if (ws != null) {
     chatAll = props.chat;
-  }  
+  }
 
   useEffect(() => {
     if (ws == null) {
-      console.log('되나..?')
+      console.log("되나..?");
       ws = createWebSocket();
-      
     }
     ws.onmessage = (event) => {
-      r = searchParams.get('room'); 
+      r = searchParams.get("room");
       const message = event.data;
       console.log(message + "이거되나요??/!");
-      setChatAll(prevChat => [...prevChat, message]);
+      setChatAll((prevChat) => [...prevChat, message]);
 
-      console.log(chatAll.length)
-
+      console.log(chatAll.length);
     };
   }, [ws]);
 
   useEffect(() => {
-    console.log(chatAll.length+"야호오")
+    console.log(chatAll.length + "야호오");
     console.log(r);
     if (chatAll.length > 0) {
-      const [nickname, time, roomNumber, text] = chatAll[chatAll.length - 1].split('*');
-      if(roomNumber.trim() != r){
-        fetch(`${url.backendUrl}/chatAlarm?userName=`+authSlice.username +'&r='+r)
-        .then(response => response.json())
-        .then(data =>   {
-          console.log(data.result);
-          document.getElementById('chatchat').textContent = data.result;
-        })
-        .catch(error => console.error('Error fetching user rooms:', error));
+      const [nickname, time, roomNumber, text] =
+        chatAll[chatAll.length - 1].split("*");
+      if (roomNumber.trim() != r) {
+        fetch(
+          `${url.backendUrl}/chatAlarm?userName=` +
+            authSlice.username +
+            "&r=" +
+            r
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data.result);
+            document.getElementById("chatchat").textContent = data.result;
+          })
+          .catch((error) => console.error("Error fetching user rooms:", error));
       }
-  
     }
-
-  }, [chatAll ]);
- 
-
+  }, [chatAll]);
 
   useEffect(() => {
-    fetch(`${url.backendUrl}/chatAlarm?userName=` + authSlice.username+'&r='+r)
+    fetch(
+      `${url.backendUrl}/chatAlarm?userName=` + authSlice.username + "&r=" + r
+    )
       .then((response) => response.json())
       .then((data) => {
         console.log(data.result);
         document.getElementById("chatchat").textContent = data.result;
       })
       .catch((error) => console.error("Error fetching user rooms:", error));
-  }, [r])
-
+  }, [r]);
 
   useEffect(() => {
-    fetch(`${url.backendUrl}/chatAlarm?userName=` + authSlice.username +'&r='+r)
+    fetch(
+      `${url.backendUrl}/chatAlarm?userName=` + authSlice.username + "&r=" + r
+    )
       .then((response) => response.json())
       .then((data) => {
         console.log(data.result);
@@ -86,10 +87,6 @@ const Header = (props) => {
       })
       .catch((error) => console.error("Error fetching user rooms:", error));
   }, []);
-
-
-
-
 
   const navigate = useNavigate();
 
@@ -127,7 +124,7 @@ const Header = (props) => {
                 </Link>
               </>
             )}
-            <Link to="/user/register">
+            <Link to="/user/terms">
               <img src="/images/join_50.png" alt="user" />
               <p>회원가입</p>
             </Link>
