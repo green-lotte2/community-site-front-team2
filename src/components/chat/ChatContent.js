e6e85a33c91e36faab0c1b53fa955a4691a38c20
gahui <gahui446@gmail.com>
2024-05-29 16:56:32 +0900

    commit

--- src/components/chat/ChatContent.js
+++ src/components/chat/ChatContent.js
@@ -201,6 +201,60 @@ const openMemberHandler = (e)=>{
     }
   }
 
+  //멤버 초대
+  const [inviteModalIsOpen, setInviteModalIsOpen ] = useState(false);
+  
+  const styles = {
+    container: {
+      maxWidth: '400px',
+      margin: '10px auto',
+      padding: '20px',
+      border: '1px solid #ccc',
+      borderRadius: '5px',
+    },
+    form: {
+      display: 'flex',
+      flexDirection: 'column',
+    },
+    input: {
+      padding: '10px',
+      marginBottom: '50px',
+      border: '1px solid #ccc',
+      borderRadius: '5px',
+    },
+    button: {
+      padding: '13px',
+      backgroundColor: '#007bff',
+      color: '#fff',
+      border: 'none',
+      borderRadius: '5px',
+      cursor: 'pointer',
+    },
+  };
+
+  const [roomName, setRoomName] = useState('');
+
+  const handleInputChange = (e) => {
+    setRoomName(e.target.value);}
+
+  const submitHandler = (e)=>{
+      e.preventDefault();
+      if(window.confirm(`${roomName} 으로 생성하시겠습니까?`)){
+        fetch('http://localhost:8080/community/chatRegister?userId='+authSlice.username+'&chatName='+roomName)
+        .then(response => response.json())
+        .then(data =>   {
+          console.log(data.result);
+         if(data.result != null){
+            alert('생성되었습니다.')
+            navigate(`/chat?room=${data.result}`)
+         }
+        })
+        .catch(error => console.error('Error fetching user rooms:', error));
+      }
+     
+  }
+
+
   return (
    
     <>
@@ -345,6 +399,30 @@ const openMemberHandler = (e)=>{
             <p key={index}>{user.name}</p>
           ))}
    
+      </Modal>
+
+      <Modal
+       isOpen={inviteModalIsOpen}
+       onRequestClose={() => setInviteModalIsOpen(false)}
+       >
+      <div id='content'> 
+    <br/>
+    <br/>
+    <br/>
+    <div style={styles.container}>
+      <h2>채팅방 만들기</h2>
+      <form style={styles.form} onSubmit={submitHandler}>
+      <br/>
+      <br/>
+        <label htmlFor="roomName">채팅방 이름:</label>
+        <input type="text" id="roomName" name="roomName"   onChange={handleInputChange}  style={styles.input} />
+        <button type="submit" style={styles.button}>채팅방 생성</button>
+      </form>
+    </div>
+    <br/>
+    <br/>
+    <br/>
+    </div>
       </Modal>
 
     </>
