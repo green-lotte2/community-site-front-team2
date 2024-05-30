0a9b141af7c0ab83bd64da78273b19e477704715
yoon <y01044285848@gmail.com>
2024-05-30 17:04:15 +0900

    일정 수정, 삭제 추가

--- src/components/calendar/SideBar.js
+++ src/components/calendar/SideBar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import moment from 'moment';
 import SideBarContents from './SideBarContents';
@@ -13,31 +13,111 @@ const SideBar = ({ rightSideHandlerClose, scheduleInfo, targetEvent, calendarObj
     location: '',
     start: moment(scheduleInfo.start).format().split('+')[0],
     end: moment(scheduleInfo.end).format().split('+')[0],
-  })
+  });
+  const [updates, setUpdates] = useState({
+    id: 0,
+    uid: authSlice.username,
+    title: '',
+    location: '',
+    start: moment(scheduleInfo.start).format().split('+')[0],
+    end: moment(scheduleInfo.end).format().split('+')[0],
+  });
 
-  const submitHandler = (e) => {
-    e.preventDefault();
-    
-    axios.post(url.backendUrl+'/calendar/insert', inputs)
-    .then((Response)=>{
-      console.log(Response.data);
-      let event = [];
-      event.push(Response.data);
-      calendarObj(1);
-    }).catch((Error)=>{
-      console.log(Error);
-    });
-  }
+  const [target, setTarget] = useState(targetEvent);
+
+  const updateRef = useRef({
+    id: 0,
+    uid: authSlice.username,
+    title: '',
+    location: '',
+    start: moment(scheduleInfo.start).format().split('+')[0],
+    end: moment(scheduleInfo.end).format().split('+')[0],
+  });
 
   const onInputChange = (e) => {
     const { value, name } = e.target;
-    console.log(name);
-    console.log(value);
+
     setInputs({
       ...inputs,
-      [name]:value,
+      [name]: value,
     })
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault();
     console.log(inputs);
+    axios.post(url.backendUrl + '/calendar/insert', inputs)
+      .then((Response) => {
+        console.log(Response.data);
+        let event = [];
+        event.push(Response.data);
+        calendarObj(1);
+      }).catch((Error) => {
+        console.log(Error);
+      });
+
+  }
+
+  const onUpdateChange = (e) => {
+    const { value, name } = e.target;
+    const key = e.target.dataset.name;
+    let index = 0;
+    target.map((event) => {
+      if (event.id == key) {
+        console.log(event);
+        target[index] = {
+          ...event,
+          [name]: value,
+        };
+        console.log(event);
+      }
+      index += 1;
+    });
+    setTarget(target);
+    console.log(target);
+  }
+
+  const updateBtnHandler = (e) => {
+
+    const targetId = e.target.value;
+    target.map((event) => {
+      if (event.id == targetId) {
+        console.log(event);
+
+        updateRef.current = {
+          id: event.id,
+          uid: authSlice.username,
+          title: event.title,
+          location: event.location,
+          start: moment(event.start).format().split('+')[0],
+          end: moment(event.end).format().split('+')[0],
+        };
+      }
+    });
+    console.log(updateRef.current);
+    axios.post(url.backendUrl + '/calendar/insert', updateRef.current)
+      .then((Response) => {
+        console.log(Response.data);
+        calendarObj(1);
+      }).catch((Error) => {
+        console.log(Error);
+      });
+
+  }
+
+  const deleteBtnHandler = (e) => {
+    const targetId = parseInt(e.target.value);
+    console.log(targetId);
+    if (window.confirm('삭제 하시겠습니까?')) {
+      axios.delete(url.backendUrl + '/calendar/delete?delId=' + targetId)
+        .then((Response) => {
+          console.log(Response.data);
+          calendarObj(1);
+        }).catch((Error) => {
+          console.log(Error);
+        });
+    }
+    
   }
 
   useEffect(() => {
@@ -55,6 +135,7 @@ const SideBar = ({ rightSideHandlerClose, scheduleInfo, targetEvent, calendarObj
   }, []);
 
   return (
+
     <div className='scheduleBox' onClick={rightSideHandlerClose}>
       <div className='scheduleContent'>
         <div className='insertForm'>
@@ -75,13 +156,13 @@ const SideBar = ({ rightSideHandlerClose, scheduleInfo, targetEvent, calendarObj
           {targetEvent.map((event) => {
             return (
               <div className='event'>
-                <h4>{event['title']}</h4>
-                <input type='select'>
-
-                </input>
+                <input name='title' type='text' data-name={event.id} onChange={onUpdateChange} defaultValue={event['title']} />
+                <input name='location' type='text' data-name={event.id} onChange={onUpdateChange} defaultValue={event['location']} />
+                <button style={{ width: "25px", height: "25px" }} value={event.id} onClick={deleteBtnHandler}>삭제</button>
+                <button style={{ width: "25px", height: "25px" }} value={event.id} onClick={updateBtnHandler}>수정</button>
                 <p className='date'>
-                  <input type='datetime-local' defaultValue={moment(event['start']).format().split('+')[0]} />
-                  <input type='datetime-local' defaultValue={moment(event['end']).format().split('+')[0]} />
+                  <input type='datetime-local' data-name={event.id} name='start' onChange={onUpdateChange} defaultValue={moment(event['start']).format().split('+')[0]} />
+                  <input type='datetime-local' data-name={event.id} name='end' onChange={onUpdateChange} defaultValue={moment(event['end']).format().split('+')[0]} />
                 </p>
               </div>
             )
