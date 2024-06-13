0d81b1fa652bf7c16ded5710ff76a81c69398c80
dpsk08270 <dpsk08270@gmail.com>
2024-06-13 17:54:43 +0900

    게시판 댓글

--- src/components/board/CommentList.js
+++ src/components/board/CommentList.js
@@ -1,12 +1,19 @@
 import React from "react";
 
 const CommentList = ({ comments }) => {
+  if (comments.length === 0) {
+    return <div>Loading...</div>; // 댓글 목록을 가져오는 중인 경우 로딩 상태 표시
+  }
+
   return (
     <div className="commentList">
-      {comments.map((comment, index) => (
-        <div key={index} className="comment">
-          <span>{comment.nick}</span>
-          <p>{comment.comment}</p>
+      {comments.map((comment) => (
+        <div key={comment.cno} className="comment">
+          <img src="/images/testAccount_50.png" alt="user"></img>
+          <div>
+            <p>{comment.nick}</p>
+            <p>{comment.content}</p>
+          </div>
         </div>
       ))}
     </div>
