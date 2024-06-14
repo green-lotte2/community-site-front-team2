59d4cae99b94122a7587156db033d18e6b1784a9
kkome <exazunyan1@gmail.com>
2024-06-14 17:55:22 +0900

    24/06/14 kkome

--- src/components/main/Example.js
+++ src/components/main/Example.js
@@ -1,10 +1,47 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import url from "../../config/url";
+import authSlice from "../../slices/authSlice";
+import { useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Example = () => {
+  const location = useLocation();
+  const authSlice = useSelector((state) => state.authSlice) || {};
+
+  const [user, setUser] = useState(
+    location.state
+      ? location.state.user
+      : {
+          uid: "",
+          image: "",
+        }
+  );
+
+  useEffect(() => {
+    axios
+      .get(url.backendUrl + "/main/" + authSlice.username)
+      .then((res) => {
+        console.log(res.data);
+        setUser({
+          ...res.data,
+        });
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+    console.log(user);
+  }, []);
+
+  console.log("이거 띄워라" + JSON.stringify(user));
+
   return (
     <>
       <div>
-        <p>안녕하세요 예시입니다.</p>
+        <img
+          src={`${url.backendUrl}/images/${authSlice.userImg}`}
+          alt="profile"
+        />
       </div>
     </>
   );
