81a8400dfc4fc431065097457953c7bfc49f05cd
yoon <y01044285848@gmail.com>
2024-06-18 17:24:43 +0900

    페이지 수정

--- src/pages/page/Page.js
+++ src/pages/page/Page.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import DefaultLayout from '../../layouts/DefaultLayout';
 import EditorComponent from '../../components/page/EditorComponent';
+import SideBarComponent from '../../components/page/SideBarComponent';
 import "../../styles/page.scss";
 const Page = () => {
     return (
         <DefaultLayout>
+            <div id='page'>
+            <SideBarComponent/>
             <EditorComponent/>
+            </div>
+            
         </DefaultLayout>
     )
 }
